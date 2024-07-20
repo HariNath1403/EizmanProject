3a339ee4e43680c53b3d7daf44e2aa885ac4ae32
Harivin <mohanesen@gmail.com>
2024-07-21 07:11:17 +0800

    f

--- app.py
+++ app.py
@@ -224,4 +224,6 @@ def generate_wb(fulldict, output_path, report_date):
     print('A new Excel document has been saved')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
